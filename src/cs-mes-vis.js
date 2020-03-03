972f7583eb29a3d50114a76eaa7bf1f0852b1a65
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2020-03-04 01:28:55 +0200

    Refactors code to CSMesVisBootstrapper

--- src/cs-mes-vis.js
+++ src/cs-mes-vis.js
@@ -2,7 +2,11 @@ const appName = "Client-Server Messaging Visualizer";
 
 const classVisualization = ".csmv-visualization";
 
-const dataKeyEnv = "environment";
+const attrVisualizationName = "cmsv-name";
+
+const dataKeyVisName = "name";
+const dataKeyVisEnv = "environment";
+const dataKeyVisActors = "actors";
 
 const errMsgVisDataNotGiven = "Configuration using CSMesVis.setupData is missing.";
 
@@ -12,8 +16,8 @@ const CSMesVis = function(container, setupData) {
   this.setupData = setupData;
 }
 
-CSMesVis.prototype.setupData;
 CSMesVis.prototype.container;
+CSMesVis.prototype.setupData;
 
 CSMesVis.prototype.init = function() {
   this.createFrame();
@@ -48,42 +52,59 @@ CSMesVisError.prototype.ensureThatEndsWithPeriod = function(s) {
 
 
 
-$( document ).ready(function() {
-  if (CSMesVis.setupData == null) {
+const CSMesVisBootstrapper = function(setupData) {
+  this.setupData = setupData;
+}
+
+CSMesVisBootstrapper.prototype.execute = function() {
+  if (this.setupData == null) {
     throw new CSMesVisError(errMsgVisDataNotGiven);
   }
-  if (!Array.isArray(CSMesVis.setupData)) {
+  if (!Array.isArray(this.setupData)) {
     throw new CSMesVisError("Incorrect setup data: The root element must be an array.");
   }
 
   const allVisualizationElements = $(classVisualization);
-  if (allVisualizationElements.length != CSMesVis.setupData.length) {
-    throw new CSMesVisError(
-                  "Incorrect setup data: There are " + allVisualizationElements.length + 
-                  " visualization(s) in the HTML file but setup data is given for " + 
-                  CSMesVis.setupData.length + " visualiation(s).");
+  if (allVisualizationElements.length != this.setupData.length) {
+    const msg = "Incorrect setup data: There are " + allVisualizationElements.length + 
+                " visualization(s) in the HTML file but setup data is given for " + 
+                this.setupData.length + " visualiation(s).";
+
+    throw new CSMesVisError(msg);
   }
 
-  CSMesVis.setupData.forEach(function(visualizationSetup, idx) {
-    if (!visualizationSetup.hasOwnProperty("name")) {
-      throw new CSMesVisError("Incorrect setup data: " + (idx + 1) + ". visualization does not have a name.");
+  this.setupData.forEach(function(visualizationSetup, idx) {
+    if (!visualizationSetup.hasOwnProperty(dataKeyVisName)) {
+      const msg = "Incorrect setup data: " + (idx + 1) + ". visualization does not have a name.";
+      throw new CSMesVisError(msg);
     }
 
     console.log(visualizationSetup.name);
 
-    const visualizationElements = $(classVisualization + "[cmsv-name='" + visualizationSetup.name + "']");
+    const visualizationElements = $(classVisualization + 
+                "[" + attrVisualizationName + "='" + visualizationSetup.name + "']");
+
     if (visualizationElements.length === 0) {
-      throw new CSMesVisError("Incorrect setup: Setup data is given for visualization '" + visualizationSetup.name + "', but the HTML file does not contain a container element for it.");
+      const msg = "Incorrect setup: Setup data is given for visualization '" +  visualizationSetup.name + 
+                  "', but the HTML file does not contain a container element for it.";
+      throw new CSMesVisError(msg);
     }
     if (visualizationElements.length > 1) {
-      throw new CSMesVisError("Incorrect setup: the HTML file contains multiple container element for visualization '" + visualizationSetup.name + "'.");
+      const msg = "Incorrect setup: the HTML file contains multiple container element for visualization '" + 
+                  visualizationSetup.name + "'."
+      throw new CSMesVisError(msg);
     }
     const visualizationElement = visualizationElements[0];
-    
     console.log(visualizationElement);
     
     const V = new CSMesVis(visualizationElement, visualizationSetup);
     V.init();
   });
+}
 
+
+
+$( document ).ready(function() {
+  const booter = new CSMesVisBootstrapper(CSMesVis.setupData);
+  booter.execute();
 });
