6a4fa7664514988e9783bc629df22e3713177f1a
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2020-03-07 16:15:01 +0200

    Adds ES6->ES5 compiling and minifying using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
 const { src, dest, series, parallel } = require('gulp');
 const babel = require('gulp-babel');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
 const del = require('del');
-const rename = require('gulp-rename');
 const jshint = require('gulp-jshint');
+const log = require('gulplog');
+const rename = require('gulp-rename');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const streamify = require('gulp-streamify');
+const uglify = require('gulp-uglify');
 
 const srcDir = "src/";
 const targetDir = "target/";
-const minifiedDir = targetDir + "minified/";
+const esFiveDir = targetDir + "es5/";
+const esFiveBabelDir = esFiveDir + "babel/";
+const esFiveDistributionDir = esFiveDir + "dist/";
 
 const extJS = ".js"
 const extMinJS = ".min" + extJS;
 
 const globAllJS = "*" + extJS;
 const globAllMinJS = "*" + extMinJS;
+const primaryJSFile = "main" + extJS;
+const esFiveDistroJSFile = "cs-mes-vis" + extMinJS;
 
-
-function jsHint() {
-  return src("./src/**/*.js")
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-}
 
 function clean() {
   return del([targetDir]);
@@ -29,14 +34,32 @@ function cssMinify(cb) {
   cb();
 }
 
-function jsBundle(cb) {
-  cb();
+function jsHint() {
+  return src([srcDir + globAllJS, srcDir + "**/" + globAllJS])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 }
 
-function jsMinify() {
-  return src(srcDir + globAllJS)
-    .pipe(rename({ extname: extMinJS }))
-    .pipe(dest(minifiedDir));
+function jsBabel() {
+  return src([srcDir + globAllJS, srcDir + "**/" + globAllJS])
+    .pipe(babel())
+    .pipe(dest(esFiveBabelDir));
+}
+
+function jsBundle() {
+  const bundleStream = browserify({
+    entries: [esFiveBabelDir + primaryJSFile],
+    debug: true
+  }).bundle();
+
+  return bundleStream
+    .pipe(source(esFiveDistroJSFile))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(streamify(uglify()))
+      .on('error', log.error)
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(esFiveDistributionDir));
 }
 
 function images(cb) {
@@ -55,7 +78,7 @@ function publish(cb) {
 exports.clean = clean;
 exports.css = series(cssMinify)
 exports.images = images
-exports.js = series(jsHint, jsBundle, jsMinify)
+exports.js = series(jsHint, jsBabel, jsBundle)
 exports.jshint = jsHint;
 exports.unittest = unitTest;
 exports.test = series(unitTest);
