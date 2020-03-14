b35a17e2e5bedbf6c1d68d3d1917dc5be0a79ea8
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2020-03-14 14:24:08 +0200

    Adds more tests for StringUtils

--- test/StringUtils.test.js
+++ test/StringUtils.test.js
@@ -6,7 +6,7 @@ import StringUtils from "../src/StringUtils.js";
 describe("Class StringUtils", () => {
   const su = new StringUtils();
 
-  const combinations = [
+  const ensureThatEndsWithCombinations = [
     //  end,      start
     [   "AAA",    "start"],
     [   "A",      "start"],
@@ -14,21 +14,74 @@ describe("Class StringUtils", () => {
     [   "AAA",    ""],
   ];
     
-  describe.each(combinations)(
-        ".ensureThatEndsWith(..., \"%s\")",
+  describe.each(ensureThatEndsWithCombinations)(
+        ".ensureThatEndsWith(..., ...)",
         (end, start) => {
 
     test(`should append "${end}" to "${start}" to get "${start+end}"`, () => {
-      expect(su.ensureThatEndsWith(start, end)).toBe(start + end);
+      expect(su.ensureThatEndsWith(start, end)).toEqual(start + end);
     });
 
     test(`should not append "${end}" to "${start+end}" to get "${start+end+end}"`, () => {
-      expect(su.ensureThatEndsWith(start + end, end)).toBe(start + end);
+      expect(su.ensureThatEndsWith(start + end, end)).toEqual(start + end);
     });
   });
 
-  test(`.ensureThatEndsWith("AAA") should return "AAAundefined"`, () => {
-    expect(su.ensureThatEndsWith("AAA")).toBe("AAAundefined");
+  test(`.ensureThatEndsWith("start") should return "startundefined"`, () => {
+    expect(su.ensureThatEndsWith("start")).toEqual("startundefined");
+  });
+
+  test(`.ensureThatEndsWithPeriod("start") should return "start."`, () => {
+    expect(su.ensureThatEndsWithPeriod("start")).toEqual("start.");
+  });
+
+  test(`.ensureThatEndsWithSpace("start") should return "start "`, () => {
+    expect(su.ensureThatEndsWithSpace("start")).toEqual("start ");
+  });
+
+
+  const isPrimitiveStringValues = [
+    //  argument,   expected
+    [   "A",        true],
+    [   " ",        true],
+    [   "",         true],
+    [   3,          false],
+    [   true,       false],
+    [   {},         false],
+    [   [1, 2],     false],
+    [   undefined,  false],
+    [   null,       false],
+  ];
+    
+  describe.each(isPrimitiveStringValues)(
+        ".isPrimitiveString(...)",
+        (arg, expected) => {
+
+    const prettyArg = typeof arg === "string" ? "\"" + arg + "\"" : arg;
+
+    test(`should return ${expected} for ${prettyArg}`, () => {
+      expect(su.isPrimitiveString(arg)).toEqual(expected);
+    });
+  });
+  
+  
+  describe.each([
+    //  argument,   expected
+    [   "A",        true],
+    [   " ",        false],
+    [   " \t ",     false],
+    [   " \v ",     false],
+    [   " \n ",     false],
+    [   " \f ",     false],
+    [   " \r ",     false],
+    [   "",         false],
+    [   3,          false],
+  ])(".isNonEmptyString(...)", (arg, expected) => {
+    const prettyArg = typeof arg === "string" ? "\"" + arg + "\"" : arg;
+
+    test(`should return ${expected} for ${prettyArg}`, () => {
+      expect(su.isNonEmptyString(arg)).toEqual(expected);
+    });
   });
 
 });
